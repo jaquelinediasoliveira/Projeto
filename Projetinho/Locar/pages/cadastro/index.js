6cd04fcfcf22ff409d03712157a39fec2bc09f27
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2021-10-25 12:23:56 -0200

    Att 25/10

--- Projetinho/Locar/pages/cadastro/index.js
+++ Projetinho/Locar/pages/cadastro/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, TextInput} from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Piker, ScrollView } from 'react-native';
 import css from "./style"
 import global from '../global/style'
 
 export default function Reservas({navigation}){
     return(
         <View style={global.tela}>
-            <Text style={css.perg}>Insira seus dados</Text>
-            <TextInput placeholder="  CPF/Passaporte" style={css.input}/>
-            <TextInput placeholder="  Nome completo" style={css.input}/>
-            <TextInput placeholder="  Telefone" style={css.input}/>
-            <TextInput placeholder="  Data de nascimento" style={css.input}/>
-            <TouchableOpacity style={css.entrar}>
-                <Text style={css.text}>Entrar</Text>
-            </TouchableOpacity>
+            <View style={css.texto}>
+                <Text style={css.font}>Insira seus Dados</Text>
+            </View>
+            <View style={css.scroll}>
+                <ScrollView>
+                    <TextInput placeholder="CPF/Passaporte" style={css.input}/>
+                    <TextInput placeholder="Nome completo" style={css.input}/>
+                    <TextInput placeholder="Telefone" style={css.input}/>
+                    <TextInput placeholder="Data de nascimento" style={css.input}/>
+                    <TextInput placeholder="E-mail" style={css.input}/>
+                    <View style={css.end}>
+                        <TextInput placeholder="Cidade" style={css.city}/>
+                        <TextInput placeholder="UF" style={css.uf}/>
+                    </View>
+                    <TextInput placeholder="Logradouro" style={css.input}/>
+                    <TextInput placeholder="Bairro" style={css.input}/>
+                    <TouchableOpacity style={css.entrar}>
+                        <Text style={css.botao}>Entrar</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
         </View>
     )
 }
--- Projetinho/Locar/pages/cadastro/style.js
+++ Projetinho/Locar/pages/cadastro/style.js
@@ -1,16 +1,59 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, StatusBar} from 'react-native';
 
 export default StyleSheet.create({
-    input:{
-        width: '75%',
-        height: '8%',
+    font: {
+        fontSize: 20,
+    },
+    scroll: {
+        height: '90%',
+        width: '90%',
+    },
+    texto: {
+        height: '10%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        marginTop: '5%',
         borderWidth: 1,
         borderColor: '#9932CC',
         borderRadius: 5,
-        marginBottom: '10%'
+        padding: 10,
     },
-    perg:{
-        marginBottom: '10%',
-        fontSize: 18
+    end: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    city: {
+        width: '72%',
+        height: 50,
+        marginTop: '2%',
+        borderWidth: 1,
+        borderColor: '#9932CC',
+        borderRadius: 5,
+        padding: 10,
+    },
+    uf: {
+        width: '25%',
+        height: 50,
+        marginTop: '2%',
+        borderWidth: 1,
+        borderColor: '#9932CC',
+        borderRadius: 5,
+        padding: 10,
+    },
+    entrar: {
+        borderRadius: 15,
+        backgroundColor: '#F7EFFA',
+        width: '40%',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: '4%',
+        marginBottom: '4%',
     }
 })
