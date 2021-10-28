46188d5616ff64b3845d0eec371fdbb8ec3b5ce9
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2021-10-28 12:17:55 -0200

    Att 28/10

--- Projetinho/Locar/pages/reservas/index.js
+++ Projetinho/Locar/pages/reservas/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, Text} from 'react-native'
 
 import Navbar from '../components/navbar'
@@ -7,6 +7,45 @@ import css from './style';
 import global from '../global/style'
 
 export default function Reservas({navigation}) {
+    // const [listaReservas, setListaReserva] = useState([]);
+
+    //useEffect(()=>{
+        // fetch('https://myapi.com')
+        // .then((response) => response.json())
+        // .then((data) => {
+        //   setListaReserva(data)
+        // })
+        // .catch((error) => {
+        //   console.error(error);
+        // });
+        const setListaReserva = 
+            [
+                {
+                    "imagem":"",
+                    "retirada":"28/10/2021",
+                    "devolucao":"29/10/2021",
+                    "valor":"R$ 120,00"
+                },
+                {
+                    "imagem":"",
+                    "retirada":"30/10/2021",
+                    "devolucao":"31/10/2021",
+                    "valor":"R$ 220,00"
+                },
+                {
+                    "imagem":"",
+                    "retirada":"32/10/2021",
+                    "devolucao":"33/10/2021",
+                    "valor":"R$ 320,00"
+                },
+                {
+                    "imagem":"",
+                    "retirada":"34/10/2021",
+                    "devolucao":"35/10/2021",
+                    "valor":"R$ 420,00"
+                }
+            ]
+
     return (
         <View style={global.tela}>
 
@@ -15,8 +54,29 @@ export default function Reservas({navigation}) {
             </View>
 
             <View style={global.body}>
+                {
+                    setListaReserva.map((item, index)=>{
+                        return(
+                            <View style={css.card}>
+                                <View style={css.imagem}>
 
+                                </View>
+                                <View>
+                                    <Text>Retirada:  </Text>
+                                    <Text>Devolução:  </Text>
+                                    <Text>Valor:  </Text>
+                                </View>
+                                <View style={css.info}>
+                                        <Text style={css.ret}>{item.retirada}</Text>
+                                        <Text style={css.dev}>{item.devolucao}</Text>
+                                        <Text>{item.valor}</Text>
+                                </View>
+                            </View>
+                        )
+                    })
+                }
             </View>
+
             <Navbar
                 navigation={navigation}
                 screen="Reservas"
--- Projetinho/Locar/pages/reservas/style.js
+++ Projetinho/Locar/pages/reservas/style.js
@@ -1,5 +1,27 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    
+    card: {
+        width: '70%',
+        height: '15%',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        padding: '0 15px',
+        flexDirection: 'row',
+        borderWidth: 1,
+        borderColor: '#9932CC',
+        borderRadius: 15,
+        marginTop: 10,
+        shadowColor: "#000",
+        shadowOffset: {width: 0, height: 2,},
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5
+    },
+    ret:{
+        color: 'green'
+    },
+    dev:{
+        color: 'red'
+    }
 })
